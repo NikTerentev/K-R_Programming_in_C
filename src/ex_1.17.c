3a322bfc64141114e4f9a83d0638d75d674d02ac
Nikita Terentyev <nikita.terentyev@saritasa.com>
2025-04-26 17:17:23 +0700

    Add ex_1.17

---
+++ src/ex_1.17.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define ROW_LEN 10
+
+int getlinenew(char s[], int lim) {
+    int c, i;
+    for (i = 0; i <= lim && (c=getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+
+    if (c == EOF)
+        return 0;
+
+    if (c == '\n')
+        return 1;
+
+    if (c != '\n') {
+        s[i] = '\0';
+        printf("%s", s);
+    }
+
+    while ((c = getchar()) != '\n') {
+        printf("%c", c);
+    }
+
+    printf("%c", c);
+
+    return 1;
+}
+
+
+int main() {
+    int c, i, len;
+    char line[ROW_LEN + 1];
+
+    while (getlinenew(line, ROW_LEN) != 0);
+
+    return 0;
+}
